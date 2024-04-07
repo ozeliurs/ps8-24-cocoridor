6b7803dc744ac1e3513b80eb69738152e6dbc3a4
krystodagues <krystoddlh@gmail.com>
2024-04-07 12:45:48 +0200

    chatbox pour la liste d'amis

--- front/FriendList/friendList.js
+++ front/FriendList/friendList.js
@@ -29,8 +29,10 @@ document.addEventListener('DOMContentLoaded', async function() {
 
             const chatButton = document.createElement("button");
             chatButton.textContent = "Chat";
+            chatButton.classList.add("chatbot-toggler");
             chatButton.onclick = function() {
-                window.location.href = `../FriendChat/index.html?friendId=${friend}`;
+                // window.location.href = `../FriendChat/index.html?friendId=${friend}`;
+                document.body.classList.toggle("show-chatbot");
             };
 
             friendElement.appendChild(chatButton);
@@ -41,3 +43,54 @@ document.addEventListener('DOMContentLoaded', async function() {
         console.error('Error:', error);
     });
 });
+
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage = null; // Variable to store user's message
+console.log(chatInput)
+console.log(chatInput.scrollHeight)
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; 
+}
+
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+}
+
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
