8ec8ba9f650387de9e1686446547407ef1db89e5
ArtanWellis <elias78210m@gmail.com>
2024-04-22 15:25:43 +0200

    ajout verif

---
+++ front/Game-Page/popup.js
@@ -0,0 +1,28 @@
+const exitBtn = document.getElementById("exit"); // Ajout de l'élément "exit"
+
+exitBtn.addEventListener("click", function() {
+    console.log("exit");
+    const popup = document.createElement("div");
+    popup.className = "popup";
+    popup.innerHTML = `
+        <div class="popup-content">
+            <p>Êtes-vous sûr de vouloir quitter ?</p>
+            <button id="confirm">Oui</button>
+            <button id="cancel">Non</button>
+        </div>
+    `;
+    document.body.appendChild(popup); // Ajout de la popup au corps du document
+
+    const confirmBtn = document.getElementById("confirm");
+    const cancelBtn = document.getElementById("cancel");
+
+    confirmBtn.addEventListener("click", function() {
+        // Rediriger l'utilisateur
+        window.location.href = '../index.html'; // Changer le chemin selon votre besoin
+        document.body.removeChild(popup); // Supprimer la popup
+    });
+
+    cancelBtn.addEventListener("click", function() {
+        document.body.removeChild(popup); // Supprimer la popup
+    });
+});
